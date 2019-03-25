d57f58f5e25700b7a7a2d8706ce9da994fb63ad4
CorentinFoucrier <foucrier.corentin@gmail.com>
2019-03-25 22:03:17 +0100

    8 validé

--- questions.js
+++ questions.js
@@ -52,7 +52,7 @@ var reverseWordsInArray = function(array) {
     var resultArray = [];
     for (i = 0; i < array.length; i++)
     {
-        var reverseWords = Array.from(array[i]).reverse().join('');
+        var reverseWords = Array.from(array[i]).reverse().join(''); //on décompose string en array, on renverse, on le rejoin l'array en string
         resultArray.push(reverseWords);
     }
     return resultArray;
@@ -72,7 +72,7 @@ var allElementsExceptFirstThree = function(array) {
 }
 
 var addElementToBeginning = function(array, element) {
-    var resultArray = [1];
+    var resultArray = [element];
     for (i = 0; i < array.length; i++)
     {
         resultArray.push(array[i]);
@@ -81,7 +81,20 @@ var addElementToBeginning = function(array, element) {
 }
 
 var sortByLastLetter = function(array) {
-    return 'Write your method here';
+    var resultArray = [];
+    for (i = 0; i < array.length; i++)
+    {
+        array.sort(function(a, b) //sort() peut avoir une fonctionComparaison
+        {
+            console.log(array); 
+            return a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1);
+//          On set a et b avec le code UTF-16 de chaque occurrence de l'array
+//          puis on passe en param de charCodeAt sa longeur -1 pour focus la dernière lettre de chaque occurrence
+        })
+        console.log(array[i]);
+        resultArray.push(array[i]);
+    }
+    return resultArray;
 }
 
 var getFirstHalf = function(string) {
